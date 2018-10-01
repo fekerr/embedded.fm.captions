6971166efa958934cebc243beae4e4d501acad96
fekerr <fekerr@gmail.com>
2018-10-01 14:06:04 -0700

    minor cleanup, delete an old comment

--- 254/doitvlc.cmd
+++ 254/doitvlc.cmd
@@ -1,6 +1,8 @@
 REM .cmd file for Windows .CMD box...
 setlocal
+set pypath=E:\Users\fred\AppData\Local\Programs\Python\Python37
+
 set captionline=%1
 if "%1" == "" set captionline=0
-python ./capscanvlc.py 254_captions.sbv embedded-ep254.mp3 %captionline%
+%pypath%\python .\capscanvlc.py 254_captions.sbv embedded-ep254.mp3 %captionline%
 
