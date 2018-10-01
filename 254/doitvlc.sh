7141220a8f7155413e89e7939eda9dc9dd40443f
fekerr <fekerr@gmail.com>
2018-10-01 14:04:11 -0700

    Add path to author's install of Python37...ugh.

---
+++ 254/doitvlc.sh
@@ -0,0 +1,6 @@
+# environmental issues on dev systems...
+# powershell, bash on windows, vlc, etc.
+
+python37_win=/mnt/e/users/fred/appdata/local/programs/python/python37/python.exe
+
+${python37_win} ./capscanvlc4.py 254_captions.sbv embedded-ep254.mp3 wordList254.txt 1
