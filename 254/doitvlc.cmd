6394472ac2d6a6a9d7d496908d1919a688adc7df
Fred Kerr <fekerr@gmail.com>
2018-10-01 19:17:25 -0700

    Add comments regarding requirements. Add path for author's laptop install of python37.

--- 254/doitvlc.cmd
+++ 254/doitvlc.cmd
@@ -1,8 +1,16 @@
 REM .cmd file for Windows .CMD box...
+
+rem Requirements:
+rem Install desktop version of VLC
+rem pip install python-vlc
+rem copy vlc.py and .mp3 to local directory (see command line below)
+
 setlocal
-set pypath=E:\Users\fred\AppData\Local\Programs\Python\Python37
+
+rem set pypath=E:\Users\fred\AppData\Local\Programs\Python\Python37
+set pypath="C:\Users\feker\AppData\Local\Programs\Python\Python37"
 
 set captionline=%1
 if "%1" == "" set captionline=0
-%pypath%\python .\capscanvlc.py 254_captions.sbv embedded-ep254.mp3 %captionline%
+%pypath%\python capscanvlc.py 254_captions.sbv embedded-ep254.mp3 %captionline%
 
